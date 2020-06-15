4f39dbd1f30fd306f92a70cb7e1fbdc80c77f492
Asha <ashalbenjamin@gmail.com>
2020-06-14 20:45:19 -0500

    server up and running

--- api/server.js
+++ api/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+// const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
+server.use(morgan('dev'));
 server.use(express.json());
+
+server.get('/', (req, res) => {
+	res.send("It's alive!");
+});
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+// server.use('/api/users', usersRouter);
 
 module.exports = server;
